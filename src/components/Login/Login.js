5ec3312204938e0d24c31dd905d1aeb28c6a1730
Aye Chan Han <ayechan.han@mo.com.mm>
2022-08-01 13:19:43 +0630

    POST request and get token

---
+++ src/components/Login/Login.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+
+async function loginUser(credentials){
+    return fetch('http://127.0.0.1:1337/api/auth/local', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(credentials)
+    }).then(data => data.json())
+}
+
+export default function Login({setToken}){
+    const [identifier, setIdentifier] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            identifier,
+            password
+        })
+        setToken(token);
+    }
+
+    return(
+        <div className='login-wrapper'>
+            <h1>Please Log In</h1>
+            <form onSubmit={handleSubmit}>
+            <label>
+                <p>Username</p>
+                <input type="text" onChange={e => setIdentifier(e.target.value)}/>
+            </label>
+            <label>
+                <p>Password</p>
+                <input type="password" onChange={e => setPassword(e.target.value)} />
+            </label>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+        </div>
+    )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
